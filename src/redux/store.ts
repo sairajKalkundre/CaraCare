11c52c460cbab448c85c463237b8db761696c903
Sairaj Kalkundre <sairajkalkundre@gmail.com>
2021-09-26 14:42:27 +0200

    #1 - Added redux-persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,25 @@
 import {configureStore} from "@reduxjs/toolkit"
 import hydrationReducer from "../redux/slices/hydrationSlice";
+import { persistReducer } from "redux-persist";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getDefaultMiddleware } from "@reduxjs/toolkit";
+
+const persistConfig = {
+    key: 'root',
+    storage : AsyncStorage
+};
+
+const persistedReducer = persistReducer(persistConfig, hydrationReducer);
+
+const customizedMiddleware = getDefaultMiddleware({
+    serializableCheck: false
+  });
 
 export const store =  configureStore({
     reducer : {
-            hydration  :  hydrationReducer
-    }
+            hydration  :  persistedReducer,
+    },
+    middleware :  customizedMiddleware
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import Home from "./Home/Home";
 import {store} from "../redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
+let persistor = persistStore(store);
 export default function Screen():React.ReactElement{
     return(
         <Provider store = {store}>
+            <PersistGate loading = {null} persistor = {persistor}>
                  <Home/>
+            </PersistGate>
         </Provider>
     )
 }
